67eb738ab87e89b7317d8574cd789f99c6e56d04
anilgyawali <anilgyawali38@gmail.com>
2026-08-14 12:15:41 +0545

    asfasf

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://ustax-nln5.vercel.app',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
 });
